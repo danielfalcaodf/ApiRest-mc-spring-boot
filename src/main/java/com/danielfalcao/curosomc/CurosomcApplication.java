3de299b4a7d42d55a89d4ab3d844651def3477c6
danielfalcaodf <danielfalcao.df@gmail.com>
2022-06-05 16:35:28 -0300

    init-project

---
+++ src/main/java/com/danielfalcao/curosomc/CurosomcApplication.java
@@ -0,0 +1,13 @@
+package com.danielfalcao.curosomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurosomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurosomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielfalcao/curosomc/CurosomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielfalcao.curosomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurosomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
