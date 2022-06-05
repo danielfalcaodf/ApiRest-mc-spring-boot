c46e42bb58bf8fe4e69b1c4d7b859df0e923b571
danielfalcaodf <danielfalcao.df@gmail.com>
2022-06-05 17:51:00 -0300

    add/Categoria-dominio

--- src/main/java/com/danielfalcao/curosomc/resources/CategoriaResource.java
+++ src/main/java/com/danielfalcao/curosomc/resources/CategoriaResource.java
@@ -1,17 +1,28 @@
 package com.danielfalcao.curosomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.danielfalcao.curosomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "Rest esta funcionando!";
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Informática");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+
+		return lista;
 	}
 
 }
