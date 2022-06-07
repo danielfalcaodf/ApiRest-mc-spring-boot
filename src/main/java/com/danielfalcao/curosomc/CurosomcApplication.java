5e5c3f113dc0c27cd76bfba45388180f20af3d24
danielfalcaodf <danielfalcao.df@gmail.com>
2022-06-06 21:54:53 -0300

    add/instaciacao-run

--- src/main/java/com/danielfalcao/curosomc/CurosomcApplication.java
+++ src/main/java/com/danielfalcao/curosomc/CurosomcApplication.java
@@ -1,13 +1,32 @@
 package com.danielfalcao.curosomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.danielfalcao.curosomc.domain.Categoria;
+import com.danielfalcao.curosomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CurosomcApplication {
+public class CurosomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurosomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat = new Categoria(null, "Informática");
+		Categoria cat1 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat, cat1));
+
 	}
 
 }
